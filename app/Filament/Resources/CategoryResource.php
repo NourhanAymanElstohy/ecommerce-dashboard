42822af93ddcb99cd8d11b9b81c804dbc27bfd52
nour7anelsto7y@gmail.com <nour7anelsto7y@gmail.com>
2024-06-16 18:06:18 +0300

    category module

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Support\Str;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255)
+                    ->afterStateUpdated(fn (string $state, callable $set) => $set('slug', Str::slug($state))),
+                Forms\Components\FileUpload::make('photo')
+                    ->directory('photos')
+                    ->image()
+                    ->maxSize(1024)
+                    ->visibility('public')
+                    ->getUploadedFileUrlUsing(fn ($record) => $record ? $record->photo_url : null),
+                Forms\Components\Textarea::make('description'),
+                Forms\Components\TextInput::make('slug')->disabled(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\ImageColumn::make('photo')
+                    ->label('Photo')
+                    ->url(fn ($record) => $record->photo_url),
+                Tables\Columns\TextColumn::make('slug'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
